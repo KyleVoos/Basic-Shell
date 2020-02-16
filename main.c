bcf4756cd55a53b6b613073dfe8c605ee8abb849
KyleVoos <43186089+KyleVoos@users.noreply.github.com>
2020-02-15 17:01:25 -0800

    see description
    Changed the way memory is being allocated in findPath
    I believe that it is correctly looking in the CWD if nothing is found in the PATH variable
    The arguments array is being cleared so now for example doing cd then ls will give the same results as ls ..
    NEED TO DO:
    still need to see if & is making things exec in the background correctly
    need to handle the cases when the user only presses enter

--- main.c
+++ main.c
@@ -5,6 +5,7 @@
 #include <unistd.h>
 #include <errno.h>
 #include <wait.h>
+#include <bsd/string.h>
 
 typedef struct _arguments {
     char *path;
@@ -19,10 +20,10 @@ void freeMem(arguments *arg) {
         free(arg->args[ii]);
         arg->args[ii] = NULL;
     }
-    free(arg->args);
-    arg->args = NULL;
-    free(arg->path);
-    arg->path = NULL;
+//    free(arg->args);
+//    arg->args = NULL;
+//    free(arg->path);
+//    arg->path = NULL;
     arg->numArgs = 0;
     arg->noWait = 0;
 }
@@ -76,14 +77,14 @@ void getArgs(char *input, arguments *arg) {
 
     token = strtok(input, delim);
     if (token[strlen(token) - 1] == '\n')
-        token[strlen(token) - 1] = '\0';
-    fprintf(stdout, "token = %s\n", token);
+        token[strcspn(token, "\n")] = '\0';
+//    fprintf(stdout, "token = %s\n", token);
     arg->args[0] = strdup(token);
     fprintf(stdout, "args[0] = %s\n", arg->args[0]);
     while ((token = strtok(NULL, delim)) != NULL) {
         if (token[strlen(token) - 1] == '\n')
-            token[strlen(token) - 1] = '\0';
-        fprintf(stdout, "token = %s\n", token);
+            token[strcspn(token, "\n")] = '\0';
+//        fprintf(stdout, "token = %s\n", token);
         if (strcmp(token, "&") == 0) {
             arg->noWait = 1;
         }
@@ -102,47 +103,53 @@ char *findPath(arguments *arg) {
     char *token = NULL;
     char *delim = ":";
     struct stat s;
-    char *execPath = NULL;
+    char *execPath = (char *) malloc(sizeof(char));
+    char temp[strlen(arg->args[0]) + 2];
+    size_t len = 0;
 
+    memset(temp, 0, sizeof(temp));
+
+    strncpy(temp, "/", strlen(arg->args[0]) + 1);
+    strncat(temp, arg->args[0], strlen(arg->args[0]) + 1);
+    temp[strlen(temp)] = '\0';
+    fprintf(stdout, "temp = %s\n", temp);
     fprintf(stdout, "PATH = %s\n", p);
     token = strtok(p, delim);
+    len = strlen(token);
     while (token != NULL) {
-        execPath = (char *) malloc(strlen(path));
+        execPath = (char *) realloc(execPath, (sizeof(char) * (len + strlen(temp) + 1)));
 //        fprintf(stdout, "token = %s\n", token);
-        strlcpy(execPath, token, sizeof(execPath));
-        strcat(execPath, "/");
+//        fprintf(stdout, "exec length = %d\n", (int) (strlen(token) + strlen(temp)));
+        strncpy(execPath, token, (len + strlen(temp)));
 //        fprintf(stdout, "execPath = %s\n", execPath);
-        strcat(execPath, arg->args[0]);
-//        fprintf(stdout, "execPath = %s\n", execPath);
+        strncat(execPath, temp, (len + strlen(temp)));
+        execPath[strlen(execPath)] = '\0';
+        fprintf(stdout, "exec = %s\n", execPath);
         if (stat(execPath, &s) != -1) {
             fprintf(stdout, "%s is in %s\n",arg->args[0], execPath);
             arg->path = strdup(execPath);
             free(p);
             free(execPath);
-            return execPath;
+            return arg->path;
         }
-//        else {
-//            fprintf(stdout, "stat == -1\n");
-//        }
-//        memset(execPath, 0, sizeof(*execPath));
-        free(execPath);
         token = strtok(NULL, delim);
+        if (token != NULL) len = strlen(token);
+        memset(execPath, 0, sizeof(*execPath));
+        fprintf(stdout, "end of while loop\n");
     }
-//    fprintf(stdout, "GOT HERE\n");
-//    memset(execPath, 0, sizeof(*execPath));
-//    fprintf(stdout, "after memset\n");
-//    cwd = getcwd(NULL, 0);
-//    fprintf(stdout, "cwd = %s\n", cwd);
-//    strlcat(cwd, arg, sizeof(cwd));
-//      execPath = "./";
-//      fprintf(stdout, "execPath = %s\n", execPath);
-//      strlcat(execPath, arg->args[0], sizeof(execPath));
-    if (stat(arg->args[0], &s) != -1) {
+    fprintf(stdout, "GOT HERE\n");
+
+    char cwd[strlen(arg->args[0]) + 3];
+    strncpy(cwd, "./", strlen(arg->args[0]) + 2);
+    strncat(cwd, arg->args[0], strlen(arg->args[0]) + 2);
+    cwd[strlen(cwd)] = '\0';
+    fprintf(stdout, "cwd = %s\n", cwd);
+    if (stat(cwd, &s) != -1) {
         fprintf(stdout, "%s is in %s\n",arg->args[0], getcwd(NULL, 0));
-        arg->path = strdup(execPath);
+        arg->path = strdup(cwd);
         return execPath;
     }
-//    free(execPath);
+    free(execPath);
     free(p);
     fprintf(stdout, "ERROR: %s not found!\n", arg->args[0]);
 
@@ -150,41 +157,43 @@ char *findPath(arguments *arg) {
 }
 
 int main(int argc, char *argv[]) {
-   char *path = NULL;
    char *line = NULL;
    size_t len = 0;
    char *cwd;
    arguments *args = (arguments *) malloc(sizeof(arguments));
-   args->args = (char **) malloc(sizeof(char *));
+   args->args = (char **) malloc(6 * sizeof(char *));
 
     if (argc == 1) {
-#pragma clang diagnostic push
-#pragma clang diagnostic ignored "-Wmissing-noreturn"
         while (1) {
             args->noWait = 0;
-            cwd = getcwd(NULL, 0);
-            fprintf(stdout, "ehs: %s> ", cwd);
+//            cwd = getcwd(NULL, 0);
+            fprintf(stdout, "ehs> ");
             getline(&line, &len, stdin);
-            getArgs(line, args);
-            if (strcmp(args->args[0], "exit") == 0) {
-                free(line);
-                free(cwd);
-                freeMem(args);
-                free(args);
-                return 0;
-            }
-            if (strcmp(args->args[0], "cd") == 0) {
-                changeDir(*args);
-            }
-            else if ((path = findPath(args)) != NULL) {
-//                fprintf(stdout, "got here\n");
-                fprintf(stdout, "path = %s\n", path);
-                execCMD(*args);
+            if (strcmp(&line[0], "\n") != 0) {
+                fprintf(stdout, "GOT HERE\n");
+                getArgs(line, args);
+                if (strcmp(args->args[0], "exit") == 0) {
+                    free(line);
+//                    free(cwd);
+                    freeMem(args);
+                    free(args->args);
+                    args->args = NULL;
+                    free(args);
+                    return 0;
+                }
+                if (strcmp(args->args[0], "cd") == 0) {
+                    changeDir(*args);
+                    freeMem(args);
+                } else if ((args->path = findPath(args)) != NULL) {
+                    fprintf(stdout, "args->path = %s\n", args->path);
+                    execCMD(*args);
+                    freeMem(args);
+                    free(args->path);
+                }
             }
             free(line);
-            free(cwd);
+//            free(cwd);
         }
-#pragma clang diagnostic pop
     }
     return 0;
 }
